465c5e600aceda10640be9a04a184107e1cb8dc1
Akash Kumar Singh <akashchouhan1516@gmail.com>
2026-04-03 15:19:16 +0530

    fix routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,4 +1,6 @@
-const router = require("express").Router();
+const express = require("express");
+const router = express.Router();
+
 const { register, login } = require("../controllers/authController");
 
 router.post("/register", register);
